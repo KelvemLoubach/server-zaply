d7491a3daa9bda9c02ad653187e0e683104b03c0
Kelvem Loubach <kelvemloubach@outlook.com>
2025-03-01 11:31:18 -0300

    978645785

--- src/services/chatDeepSeeak.ts
+++ src/services/chatDeepSeeak.ts
@@ -19,7 +19,7 @@ const randomDelay = (min: number, max: number): Promise<void> => {
 export const getDeepseekResponse = async (history: any[], userMessage: string) => {
   try {
     const messages = [
-      { role: "system", content: `Se basei nesse texto ${process.env.CHAT_BASE} e continue a conversa. Responda como um humano, direto e conciso, de modo não muito formal, com frases curtas. Evite usasr 'estou aqui para ajudar, como posso ajudar, se precisar de mais alguma coisa', lembre-se, você não é uma IA você é um humano.` },
+      { role: "system", content: `Continue a conversa. Responda como um humano, direto e conciso, de modo não muito formal, com frases curtas. Evite usasr 'estou aqui para ajudar, como posso ajudar, se precisar de mais alguma coisa', lembre-se, você não é uma IA você é um humano.` },
       ...history,
       { role: "user", content: userMessage }
     ];
